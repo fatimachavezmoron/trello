dc772a4f932b16b0c4d672fc8caa14291e1d7496
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-09-28 21:14:38 +0200

    install prisma

--- app/(platform)/(dashboard)/organization/[organizationID]/settings/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationID]/settings/page.tsx
@@ -1,5 +1,27 @@
+// pages/organization/[orgId]/settings/[[...rest]].tsx
+import { OrganizationProfile } from "@clerk/nextjs";
+
 const SettingsPage = () => {
-  return <div>Settings!</div>;
+  return (
+    <div>
+      <OrganizationProfile
+        routing="hash"
+        appearance={{
+          elements: {
+            rootBox: {
+              boxShadow: "none",
+              width: "100%",
+            },
+            card: {
+              border: "1px solid #e5e5e5",
+              boxShadow: "none",
+              width: "100%",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default SettingsPage;
