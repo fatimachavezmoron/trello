3f8bf77b5b09d04a1c578c6ecb034f65bab231e7
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-03 15:46:58 +0200

    show errors

--- app/(platform)/(dashboard)/organization/[organizationID]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationID]/board.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import { Button } from "@/components/ui/button";
 import { deleteBoard } from "@/actions/delete-board";
 
 interface BoardProps {
