09298e086bf02cf4871041862df6c4161878833b
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-06 14:41:16 +0200

    form errors component

---
+++ components/ui/form/form-errors.tsx
@@ -0,0 +1,20 @@
+import { XCircle } from "lucide-react";
+
+interface FormErrorsProps {
+  id: string;
+  errors?: Record<string, string[] | undefined>;
+}
+
+export const FormErrors = ({ id, errors }: FormErrorsProps) => {
+  if (!errors) {
+    return null;
+  }
+
+  return (
+    <div
+      id={`${id}-error`}
+      aria-live="polite"
+      className="mt-2 text-xs text-rose-500"
+    ></div>
+  );
+};
