fce9b5c96c4b7b66dbddca2ae2e9698b4b09599f
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-06 14:49:24 +0200

    form errors component finished

--- components/ui/form/form-errors.tsx
+++ components/ui/form/form-errors.tsx
@@ -15,6 +15,15 @@ export const FormErrors = ({ id, errors }: FormErrorsProps) => {
       id={`${id}-error`}
       aria-live="polite"
       className="mt-2 text-xs text-rose-500"
-    ></div>
+    >
+      {errors?.[id]?.map((error: string) => (
+        <div
+          key={error}
+          className="flex items-center font-medium p-2 border border-rose-500 bg-rose-500/10 rouded-sm"
+        >
+          <XCircle className="h-4 w-4 mr-2" />
+        </div>
+      ))}
+    </div>
   );
 };
