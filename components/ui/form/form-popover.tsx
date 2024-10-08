821d44140ceb007774b7d2425f15da92166adfbf
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-08 21:42:39 +0200

    input on popover

--- components/ui/form/form-popover.tsx
+++ components/ui/form/form-popover.tsx
@@ -1,14 +1,16 @@
 "use client";
 import {
   Popover,
+  PopoverClose,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-
+import { Button } from "@/components/ui/button";
 import { useAction } from "@/hooks/use-action";
 import { CreateBoard } from "@/actions/create-board/schema";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { X } from "lucide-react";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -35,6 +37,19 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form>
+          <div className="space-y-4">
+            <FormInput id="title" label="Board title" type="text" />
+          </div>
+        </form>
       </PopoverContent>
     </Popover>
   );
