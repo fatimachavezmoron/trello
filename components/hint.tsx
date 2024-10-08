d202a2beeee8d1f8c44d7c4f1b350430f4508a64
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-08 21:33:19 +0200

    popover component

--- components/hint.tsx
+++ components/hint.tsx
@@ -18,16 +18,18 @@ export const Hint = ({
   side = "bottom",
   sideOffset = 0,
 }: HintProps) => {
-  <TooltipProvider>
-    <Tooltip delayDuration={0}>
-      <TooltipTrigger>{children}</TooltipTrigger>
-      <TooltipContent
-        sideOffset={sideOffset}
-        side={side}
-        className="text-xs max-w-[220px] break-words"
-      >
-        {description}
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>;
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="text-xs max-w-[220px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
