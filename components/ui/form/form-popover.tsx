3049e804f55e5e7d828a321fad051e5268e5e107
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-09 19:04:04 +0200

    unsplash key for the images

--- components/ui/form/form-popover.tsx
+++ components/ui/form/form-popover.tsx
@@ -13,6 +13,7 @@ import { FormSubmit } from "./form-submit";
 import { X } from "lucide-react";
 import { createBoard } from "@/actions/create-board/index";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -66,6 +67,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form className="space-y-4" action={onSubmit}>
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
