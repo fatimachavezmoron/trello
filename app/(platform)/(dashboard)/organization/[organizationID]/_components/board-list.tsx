3780a82a748f05efb064f975c09b61ba9e7e8774
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-11 14:41:16 +0200

    skeleton for loading page in boardlist

--- app/(platform)/(dashboard)/organization/[organizationID]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationID]/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const BoardList = async () => {
   const { orgId } = auth();
@@ -52,6 +53,21 @@ export const BoardList = async () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grdi-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
     </div>
   );
 };
