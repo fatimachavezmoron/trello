328314eef5063c1785ee10fbf8f5b3de863b7ac2
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-15 22:07:05 +0200

    wallpaper on de background

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { db } from "../../../../../lib/db";
 
-const BoardIdLayout = ({ children }: { children: React.ReactNode }) => {
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
   return (
-    <div>
+    <div
+      className="h-full relative bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
