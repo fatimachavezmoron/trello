516936c660a5bfc39bc9d81db0d000216f118a40
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-10-15 23:15:20 +0200

    input form on the board name

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,15 +1,52 @@
 "use client";
-import React from "react";
+import React, { ElementRef, useRef } from "react";
 import { Board } from "@prisma/client";
 import { Button } from "../../../../../../components/ui/button";
+import { useState } from "react";
+import { FormInput } from "../../../../../../components/ui/form/form-input";
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enabledEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disabledEditing = () => {
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <form ref={formRef} className="flex items-center gap-x-2">
+        <FormInput
+          ref={inputRef}
+          id="title"
+          onBlur={() => {}}
+          defaultValue={data.title}
+          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-nonefocus-visible:ring-transparent border-none"
+        />
+      </form>
+    );
+  }
+
   return (
-    <Button className="font-bold text-lg h-auto w-auto p-1 px-2 ">
+    <Button
+      onClick={enabledEditing}
+      variant="transparent"
+      className="font-bold text-lg h-auto w-auto p-1 px-2 "
+    >
       {data.title}
     </Button>
   );
