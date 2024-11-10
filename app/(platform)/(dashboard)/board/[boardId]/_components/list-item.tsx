eb53b4c3ef8dcc2b9bc2fe1aec9b514917f51cd3
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-11-10 18:19:19 +0100

    card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -3,6 +3,7 @@
 import { ListWithCards } from "@/types";
 import { ListHeader } from "./list-header";
 import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -27,7 +28,14 @@ export const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} onAddCard={enableEditing}/>
+        <ListHeader data={data} onAddCard={enableEditing} />
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
