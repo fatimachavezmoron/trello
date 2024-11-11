1b1b6ff07224c4b8b6fb3a6909b59939fff0771d
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-11-11 21:54:17 +0100

    card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { forwardRef } from "react";
+
 interface CardFormProps {
   listId: string;
   enableEditing: () => void;
@@ -7,11 +11,22 @@ interface CardFormProps {
   isEditing: boolean;
 }
 
-export const CardForm = ({
-  listId,
-  enableEditing,
-  disableEditing,
-  isEditing,
-}: CardFormProps) => {
-  return <div>card form</div>;
-};
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    return (
+      <div className="pt-2 px-2">
+        <Button
+          onClick={enableEditing}
+          className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+          size="sm"
+          variant="ghost"
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Add a card
+        </Button>
+      </div>
+    );
+  }
+);
+
+CardForm.displayName = "CardForm";
