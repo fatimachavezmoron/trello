24b0d0f0641e716eae61d911826bcf1ff43755c3
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-11-11 22:39:27 +0100

    textarea component

---
+++ components/ui/form/form-textarea.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { forwardRef, KeyboardEventHandler } from "react";
+import { Label } from "../label";
+import { Textarea } from "../textarea";
+
+interface FormTextareaProps {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  onBlur?: () => void;
+  onClick: () => void;
+  onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
+  defaultValue?: string;
+}
+
+export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
+  (
+    {
+      id,
+      label,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      onBlur,
+      onClick,
+      onKeyDown,
+      defaultValue,
+    },
+    ref
+  ) => {
+    return (
+      <div className="space-y-2 w-full">
+        <div className="space-y-1 w-full">
+          {label ? (
+            <Label className="text-xs font-semibold text-neutral-700">
+              {label}
+            </Label>
+          ) : null}
+          <Textarea onKeyDown={onKeyDown} />
+        </div>
+      </div>
+    );
+  }
+);
+
+FormTextarea.displayName = "FormTextarea";
