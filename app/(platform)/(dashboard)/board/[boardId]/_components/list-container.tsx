05a4885473e16af081d734b126b9b83e8350925e
Fatima Chavez <fatimachavezmoron@gmail.com>
2024-11-21 17:59:46 +0100

    Added useAction hook to ListContainer to handle list reordering

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -5,6 +5,9 @@ import { ListForm } from "./list-form";
 import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { useAction } from "@/hooks/use-action";
+import { updateListOrder } from "@/actions/update-list-order";
+import { toast } from "sonner";
 
 interface ListCointainerProps {
   data: ListWithCards[];
@@ -21,6 +24,15 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 
 export const ListContainer = ({ data, boardId }: ListCointainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
+
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => {
+      toast.success("List reordered");
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   useEffect(() => {
     setOrderedData(data);
@@ -46,10 +58,11 @@ export const ListContainer = ({ data, boardId }: ListCointainerProps) => {
 
     if (type === "list") {
       const items = reorder(orderedData, source.index, destination.index).map(
-        (item, index) => ({ ...item, order: index })
+        (item, index) => ({ ...item, order: index, updatedAt: new Date() })
       );
 
       setOrderedData(items);
+      executeUpdateListOrder({ items, boardId });
     }
 
     // user moves a card
